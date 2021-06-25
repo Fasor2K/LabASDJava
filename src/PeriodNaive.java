cfe4fdd42dc19b0c35d55ad01d1e16b51d049717
Fasor2K <elia.elia2300@gmail.com>
2021-06-25 15:49:29 +0200

    Aggiunto PeriodNaive

---
+++ src/PeriodNaive.java
@@ -0,0 +1,14 @@
+public class PeriodNaive {
+    public static int calculatePeriod(String s){
+        int n=s.length();
+
+        for(int i=1;i<=n;i++){
+            String s1=s.substring(0,n-i);
+            String s2=s.substring(i,n-i);
+            if(s1.equals(s2)){
+                return i;
+            }
+        }
+        return n;
+    }
+}
